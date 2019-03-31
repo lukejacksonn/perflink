827d2fa7b28b9bc9d9fbe107a56e45468e59833b
Luke Jackson <lukejacksonn@gmail.com>
2019-03-31 21:08:55 +0100

    Use es-react-production when not on localhost

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-import { React, ReactDOM } from 'https://unpkg.com/es-react@16.8.30/index.js';
 import htm from 'https://unpkg.com/htm@2.1.1/dist/htm.mjs'
 import csz from 'https://unpkg.com/csz@0.1.2/index.js'
 
-window.React = React
-window.css = csz
-window.html = htm.bind(React.createElement)
+import(location.hostname === 'localhost'
+  ? 'https://unpkg.com/es-react@16.8.30/index.js'
+  : 'https://unpkg.com/es-react-production@16.8.30/index.js').then(app)
 
-const Route = {
-  '/': React.lazy(() => import('./routes/home/index.js')),
-  '*': React.lazy(() => import('./routes/notfound/index.js')),
+function app({ React, ReactDOM }) {
+  window.React = React
+  window.css = csz
+  window.html = htm.bind(React.createElement)
+
+  const Fallback = html`
+    <div></div>
+  `
+  const Route = {
+    '/': React.lazy(() => import('./routes/home/index.js')),
+    '*': React.lazy(() => import('./routes/notfound/index.js')),
+  }
+
+  ReactDOM.render(
+    html`
+      <${React.Suspense} fallback=${Fallback}>
+        <${Route[location.pathname] || Route['*']} />
+      <//>
+    `,
+    document.body
+  )
 }
-
-ReactDOM.render(
-  html`
-    <${React.Suspense} fallback=${html`<div></div>`}>
-      <${Route[location.pathname] || Route['*']} />
-    <//>
-  `,
-  document.body
-)
