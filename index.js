ddc88d4cb52592b298c0de4519c7f41a60c68fb8
Luke Edwards <luke.edwards05@gmail.com>
2019-04-01 18:44:11 -0700

    attach `import()` conditional helper

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import htm from 'https://unpkg.com/htm@2.1.1/dist/htm.mjs'
 import csz from 'https://unpkg.com/csz@0.1.2/index.js'
 
-import(location.hostname === 'localhost'
+// Call the native `import` if it exists.
+// This method isn't parsed initially & throws
+// "safely" in Firefox w/o killing the whole app.
+function shim(url) {
+  try {
+    return new Function(`return import('${url}')`).call();
+  } catch (tmp) {
+    // Relative ~> fully qualified URLs
+    (tmp = document.createElement('a')).href = url;
+    return __shimport__.load(tmp.href);
+  }
+}
+
+shim(location.hostname === 'localhost'
   ? 'https://unpkg.com/es-react@16.8.30/index.js'
   : 'https://unpkg.com/es-react-production@16.8.30/index.js').then(app)
 
@@ -14,8 +27,8 @@ function app({ React, ReactDOM }) {
     <div></div>
   `
   const Route = {
-    '/': React.lazy(() => import('./routes/home/index.js')),
-    '*': React.lazy(() => import('./routes/notfound/index.js')),
+    '/': React.lazy(() => shim('./routes/home/index.js')),
+    '*': React.lazy(() => shim('./routes/notfound/index.js')),
   }
 
   ReactDOM.render(
