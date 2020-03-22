26150fe464ea98d2eee8e4e4e737a237fbf0b0ea
lukejacksonn <lukejacksonn@gmail.com>
2020-03-22 21:25:51 +0000

    Runs tasks as async so await can be used

--- run.js
+++ run.js
@@ -1,11 +1,11 @@
-onmessage = function(e) {
+onmessage = async e => {
   const before = e.data[0]
   const test = e.data[1]
   const duration = e.data[2]
   let result
 
   try {
-    ops = eval(`() => {
+    ops = await eval(`async () => {
         ${before};
         let ops = 0;
         let end = Date.now() + ${duration};
@@ -29,19 +29,3 @@ onmessage = function(e) {
 
   postMessage(result)
 }
-
-// var hz, period, startTime = new Date, runs = 0;
-
-// do {
-//   // Code snippet goes here
-//   runs++;
-//   totalTime = new Date - startTime;
-// } while (totalTime < 1000);
-// // convert ms to seconds
-// totalTime /= 1000;
-// // period → how long per operation
-// period = totalTime / runs;
-// // hz → the number of operations per second
-// hz = 1 / period;
-// // can be shortened to //
-// hz = (runs * 1000) / totalTime;
