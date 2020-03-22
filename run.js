82d6b3e98e004781657da9e017258610eff1ae78
lukejacksonn <lukejacksonn@gmail.com>
2020-03-22 16:08:43 +0000

    Does more runs of a shorter duration

--- run.js
+++ run.js
@@ -7,8 +7,8 @@ onmessage = function(e) {
   try {
     ops = eval(`() => {
         ${before};
-        let end = Date.now() + ${duration};
         let ops = 0;
+        let end = Date.now() + ${duration};
         while (Date.now() < end) {
             ${test.code};
             ops++;
@@ -29,3 +29,19 @@ onmessage = function(e) {
 
   postMessage(result)
 }
+
+// var hz, period, startTime = new Date, runs = 0;
+
+// do {
+//   // Code snippet goes here
+//   runs++;
+//   totalTime = new Date - startTime;
+// } while (totalTime < 1000);
+// // convert ms to seconds
+// totalTime /= 1000;
+// // period → how long per operation
+// period = totalTime / runs;
+// // hz → the number of operations per second
+// hz = 1 / period;
+// // can be shortened to //
+// hz = (runs * 1000) / totalTime;
